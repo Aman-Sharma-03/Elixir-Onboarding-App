00c1c4ff4563596795f978d4436cfb1dc97c291f
Aman-Sharma-03 <aman.sharma2022b@gmail.com>
2025-05-17 12:14:23 +0530

    init

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 justify-center items-center"
+      // style={{
+      //   flex: 1,
+      //   justifyContent: "center",
+      //   alignItems: "center",
+      // }}
+    >
+      <Text>Onboarding App</Text>
+    </View>
+  );
+}
